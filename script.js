875810b3adeaef6128ee9afb8e40ff3467c746b4
MikeTheStrike <muzialko@gmail.com>
2023-05-31 17:07:32 +0200

    setup files

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const GameBoard = (()=>{
+    //const board = new Array(9);
+    const board = [0,1,2,3,4,5,6,7,8];
+    return board;
+  })();
+
+const Player = (name, marker)=>{
+    return{name, marker};
+}
+
+const Score = (()=>{
+
+    //funkcja co sprawdza linie
+
+    //funkjca co sprawdz czy wsztstkie pola sa uzupelnione
+
+    //funkcja co trzyma wynik  i sprawdza warunki wyniku rundy
+
+    return{      }
+})();
+
+
+
+  //gameBoard[1]=3;
+  //console.log(gameBoard)
